31089afe4249d68a442c7940d301b0376d88f0fa
Poorvi-tech <poorvisahu975@gmail.com>
2026-02-06 21:41:34 +0530

    refactor: for deployment added code in ai

--- artvista-AI/ai_suggestions/scripts/api_mock.py
+++ artvista-AI/ai_suggestions/scripts/api_mock.py
@@ -36,4 +36,5 @@ def get_art_creation_suggestions(user_id):
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
--- artvista-AI/game_logic/scripts/game_api.py
+++ artvista-AI/game_logic/scripts/game_api.py
@@ -133,4 +133,5 @@ def get_leaderboard():
     return jsonify(mock_leaderboard)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=False)
