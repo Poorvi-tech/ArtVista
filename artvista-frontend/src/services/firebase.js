f8524d14a2fe1d4d547b19ec44656b8e030e2cbd
Poorvi-tech <poorvisahu975@gmail.com>
2026-02-06 23:54:57 +0530

    fixed security issue

--- artvista-frontend/src/services/firebase.js
+++ artvista-frontend/src/services/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDb-tD6w3TWqjn9UPAGzwqGl2CXmDBWgrQ",
-  authDomain: "artvista-5c245.firebaseapp.com",
-  projectId: "artvista-5c245",
-  storageBucket: "artvista-5c245.firebasestorage.app",
-  messagingSenderId: "661811526211",
-  appId: "1:661811526211:web:6636318c648c6cd993f2f6",
-  measurementId: "G-BTHRQKJ59X"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
